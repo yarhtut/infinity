1d13cbf5da337096fc86711879956d7be9d3f486
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-07-01 11:17:53 +1200

    add before in project index rspec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe ProjectsController, type: :controller do
   before do
     create_list(:project, 2)
   end
-  
+
   describe '#index' do
-    it 'render the index template' do
+    before do
       get :index
+    end
+    it 'render the index template' do
       expect(response).to render_template :index
     end
     it 'assigns all projects to @projects' do
-      get :index
       expect(assigns(:projects)).to eq Project.all
     end
   end
