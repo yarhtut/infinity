014e2777701e9942acb6aece145aeede7aa68c56
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-07-01 10:57:05 +1200

    refactor project rspec model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   describe 'validation' do
+    let (:project) { build(:project, title: nil, description: nil) }
+
     it 'require a title' do
-      @project = build(:project, title: nil)
-      @project.valid?
-      expect(@project.errors[:title]).to include "can't be blank"
+      project.valid?
+      expect(project.errors[:title]).to include "can't be blank"
     end
 
     it 'require a description' do
-      @project = build(:project, description: nil)
-      @project.valid?
-      expect(@project.errors[:description]).to include "can't be blank"
+      project.valid?
+      expect(project.errors[:description]).to include "can't be blank"
     end
   end
 end
