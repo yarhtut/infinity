1cee3400247d087261ca7bd73a18ad5efafe8a6e
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-07-01 15:07:18 +1200

    add edit template and test for update and destroy

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -55,4 +55,80 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+
+  describe 'GET #new' do
+    before do
+      get :new
+    end
+    it 'renders the :new template' do
+      expect(response).to render_template :new
+    end
+
+    it 'assigns the new projects' do
+      expect(assigns(:project)).to be_a(Project)
+    end
+  end
+
+  describe 'PATCH #update' do
+    before do
+      @project = create(:project,
+                        title: 'Rspec', description: 'we got 16 rspec yeah!!!')
+    end
+
+    context 'with valid attribute' do
+      it 'locates the requested @project' do
+        patch :update, id: @project, project: attributes_for(:project)
+        expect(assigns(:project)).to eq(@project)
+      end
+
+      it 'updates the @project attribute' do
+        patch :update, id: @project,
+                       project: attributes_for(:project,
+                                               title: 'Rspec Updated',
+                                               description: '17 rspec!!')
+        @project.reload
+        expect(@project.title).to eq 'Rspec Updated'
+        expect(@project.description).to eq '17 rspec!!'
+      end
+
+      it 'redirect to the show page' do
+        patch :update, id: @project, project: attributes_for(:project)
+        expect(response).to redirect_to project_path(@project)
+      end
+    end
+
+    context 'with invalid attribues' do
+      it 'will not update @project attribute' do
+        patch :update, id: @project,
+                       project: attributes_for(:project,
+                                               title: 'Rspec Updated',
+                                               description: nil)
+        @project.reload
+        expect(@project.title).to_not eq 'Rspec Updated'
+        expect(@project.description).to_not eq '17 rspec!!'
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before do
+      @project = create(:project)
+    end
+
+    it 'assigns the project' do
+      delete :destroy, id: @project
+      expect(assigns(:project)).to eq @project
+    end
+
+    it 'delete the project' do
+      expect do
+        delete :destroy, id: @project
+      end.to change(Project, :count).by(-1)
+    end
+
+    it 'redirect to the index' do
+      delete :destroy, id: @project
+      expect(response).to redirect_to projects_path
+    end
+  end
 end
