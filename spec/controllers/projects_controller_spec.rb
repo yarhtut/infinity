3805cb2ddea8a39e9f014d9ead8bcee4d7ae6c6f
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-07-01 13:19:37 +1200

    clean up gem file and add project show page

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -9,13 +9,26 @@ RSpec.describe ProjectsController, type: :controller do
     before do
       get :index
     end
-    
+
     it 'render the index template' do
       expect(response).to render_template :index
     end
 
     it 'assigns all projects to @projects' do
       expect(assigns(:projects)).to eq Project.all
+    end
+  end
+
+  describe '#show' do
+    it 'render the show template' do
+      get :show, id: create(:project)
+      expect(response).to render_template :show
+    end
+
+    it 'assigns the correct project' do
+      @project = create(:project)
+      get :show, id: @project
+      expect(assigns(:project)).to eq @project
     end
   end
 end
