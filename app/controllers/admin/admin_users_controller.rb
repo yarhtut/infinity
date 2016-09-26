3e81d727ad060db4c1871fc80b11ddcbc100531b
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-09-26 22:12:29 +1300

    add spec

--- app/controllers/admin/admin_users_controller.rb
+++ app/controllers/admin/admin_users_controller.rb
@@ -1,22 +1,21 @@
-module Admin
-  class AdminUsersController < Admin::ApplicationController
-    # To customize the behavior of this controller,
-    # simply overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = AdminUser.all.paginate(10, params[:page])
-    # end
-
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   AdminUser.find_by!(slug: param)
-    # end
-
-    # See https://administrate-docs.herokuapp.com/customizing_controller_actions
-    # for more information
-
-    def index
-    end
-  end
-end
+# module Admin
+#   class AdminUsersController < Admin::ApplicationController
+#     # To customize the behavior of this controller,
+#     # simply overwrite any of the RESTful actions. For example:
+#     #
+#     # def index
+#     #   super
+#     #   @resources = AdminUser.all.paginate(10, params[:page])
+#     # end
+#
+#     # Define a custom finder by overriding the `find_resource` method:
+#     # def find_resource(param)
+#     #   AdminUser.find_by!(slug: param)
+#     # end
+#     # See https://administrate-docs.herokuapp.com/customizing_controller_actions
+#     # for more information
+#
+#     def index
+#     end
+#   end
+# end
